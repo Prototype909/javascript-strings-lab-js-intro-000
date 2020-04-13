654603ba99bf696103c6bb96db9c8d171e3104fb
Steve Anderson <prototype909@msn.com>
2020-04-13 07:02:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neil deGrasse Tyson";
  
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "topic";
+var topic = "space";
 
 var conversation = (`Lets talk about ${topic}`);
